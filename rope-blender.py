10f532d4f69024aac47f44dc16106dbc8ec97943
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2019-06-18 22:28:18 -0700

    Created a rope class for Blender, removed magic #'s

--- rope-blender.py
+++ rope-blender.py
@@ -1,62 +1,93 @@
 import bpy
 from math import *
 from mathutils import *
+import pprint
+import random
 
-# Delete any objects already in the scene
-bpy.ops.object.mode_set(mode='OBJECT')
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete()
 
-# Make the rope (based off this tutorial: https://youtu.be/xYhIoiOnPj4)
-context = bpy.context
-bpy.ops.mesh.primitive_circle_add(location=(0, 0, 0))
-bpy.ops.transform.resize(value=(0.1, 0.1, 0.1))
-bpy.ops.object.mode_set(mode='EDIT')
-bpy.ops.transform.rotate(value= pi / 2, orient_axis='X')
-bpy.ops.transform.translate(value=(0.1, 0, 0))
-bpy.ops.object.mode_set(mode='OBJECT')
-circle = context.active_object
-circle.name = "Circle0"
-for i in range(1, 4):
-    bpy.ops.object.duplicate_move(OBJECT_OT_duplicate=None, TRANSFORM_OT_translate=None)
-    ob = bpy.context.active_object
-    ob.name = "Circle%d" % i
-    ob.rotation_euler = (0, 0, i * (pi / 2))
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.join()
-rope = context.active_object
-rope.name = "Rope"
-bpy.ops.object.modifier_add(type='SCREW')
-rope.modifiers["Screw"].screw_offset = 10
-rope.modifiers["Screw"].iterations = 10
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Screw")
+class RopeRenderer:
+    def __init__(self, rope_radius=0.1, rope_screw_offset=8, rope_length=10, bezier_scale=7, bezier_subdivisions=10):
+        self.rope_radius = rope_radius
+        self.rope_length = rope_length
+        self.rope_screw_offset = rope_screw_offset
+        self.bezier_scale = bezier_scale
+        self.bezier_subdivisions = bezier_subdivisions
+        self.origin = (0, 0, 0)
+        self.rope = None
+        self.asymm_rope = None
+        self.bezier = None
+        self.rope_name = "Rope"
+        self.asymm_rope_name = "Rope-Asymmetric"
+        self.bezier_name = "Bezier"
 
-# Add a cone and a sphere at either end of the rope for asymmetry
-bpy.ops.mesh.primitive_cone_add(location=(0, 0.32, 0))
-bpy.ops.transform.resize(value=(0.5, 0.5, 0.5))
-bpy.ops.transform.rotate(value= pi / 2, orient_axis='X')
-bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(0, 0, 10))
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.join()
-rope_asymm = context.active_object
-rope_asymm.name="Rope-Asymmetric"
+    def clear(self):
+        # Delete any existing objects in the scene
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete()
 
-# Create bezier curve and attach rope to it
-bpy.ops.curve.primitive_bezier_curve_add(location=(0, 0, 0))
-bpy.ops.transform.resize(value=(7, 7, 7))
-bpy.ops.object.mode_set(mode='EDIT')
-bpy.ops.curve.subdivide(number_cuts=7)
-bpy.ops.object.mode_set(mode='OBJECT')
-bezier = context.active_object
-bezier.name = "Bezier"
-bpy.data.objects['Bezier'].select_set(False)
-bpy.data.objects['Rope-Asymmetric'].select_set(True)
-context.view_layer.objects.active = rope_asymm
-bpy.ops.object.modifier_add(type='CURVE')
-rope_asymm.modifiers["Curve"].deform_axis = 'POS_Z'
-rope_asymm.modifiers["Curve"].object = bpy.data.objects["Bezier"]
+    def make_rigid_rope(self):
+        # Join 4 circles and "twist" them to create realistic rope (based off this tutorial: https://youtu.be/xYhIoiOnPj4)
+        bpy.ops.mesh.primitive_circle_add(location=self.origin)
+        bpy.ops.transform.resize(value=(self.rope_radius, self.rope_radius, self.rope_radius))
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.transform.rotate(value= pi / 2, orient_axis='X')
+        bpy.ops.transform.translate(value=(self.rope_radius, 0, 0))
+        bpy.ops.object.mode_set(mode='OBJECT')
+        for i in range(1, 4):
+            bpy.ops.object.duplicate_move(OBJECT_OT_duplicate=None, TRANSFORM_OT_translate=None)
+            ob = bpy.context.active_object
+            ob.rotation_euler = (0, 0, i * (pi / 2))
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.join()
+        self.rope = bpy.context.active_object
+        self.rope.name = self.rope_name
+        bpy.ops.object.modifier_add(type='SCREW')
+        self.rope.modifiers["Screw"].screw_offset = self.rope_screw_offset
+        self.rope.modifiers["Screw"].iterations = self.rope_length
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Screw")
 
-# Experiment with creating knots
-# bpy.data.objects['Bezier'].select_set(True)
-# bpy.data.objects['Rope-Asymmetric'].select_set(False)
-# bpy.ops.transform.translate(value=(0.398516, 1.60635, 1.08822))
+    def add_rope_asymmetry(self):
+        # Add a cone and icosphere at either end, to break symmetry
+        bpy.ops.mesh.primitive_cone_add(location=(self.origin[0], self.origin[1] + 0.32, self.origin[2]))
+        bpy.ops.transform.resize(value=(0.5, 0.5, 0.5))
+        bpy.ops.transform.rotate(value= pi / 2, orient_axis='X')
+        bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(self.origin[0], self.origin[1], self.origin[2] + self.rope_radius*self.rope_screw_offset*self.rope_length))
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.join()
+        self.rope_asymm = bpy.context.active_object
+        self.rope_asymm.name= self.asymm_rope_name
+
+    def make_bezier(self):
+        # Create bezier curve, attach rope to it
+        bpy.ops.curve.primitive_bezier_curve_add(location=self.origin)
+        bpy.ops.transform.resize(value=(self.bezier_scale, self.bezier_scale, self.bezier_scale))
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.curve.subdivide(number_cuts=self.bezier_subdivisions)
+        bpy.ops.object.mode_set(mode='OBJECT')
+        self.bezier = bpy.context.active_object
+        self.bezier.name = self.bezier_name
+        self.bezier.select_set(False)
+        self.rope_asymm.select_set(True)
+        bpy.context.view_layer.objects.active = self.rope_asymm
+        bpy.ops.object.modifier_add(type='CURVE')
+        self.rope_asymm.modifiers["Curve"].deform_axis = 'POS_Z'
+        self.rope_asymm.modifiers["Curve"].object = self.bezier
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.normals_make_consistent(inside=False)
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    def randomize_one_node(self):
+        pass
+        # bez_points = self.bezier.data.splines[0].bezier_points
+        # sz = len(bez_points)
+        # for i in range(0, sz):
+        #     # Set every sixth coordinate's z to 0.5.
+        #     bez_points[i].co.z += random.uniform(0, 0.2)
+
+if __name__ == '__main__':
+    renderer = RopeRenderer()
+    renderer.clear()
+    renderer.make_rigid_rope()
+    renderer.add_rope_asymmetry()
+    renderer.make_bezier()
