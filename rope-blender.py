3c12421ace1ca0d2bffd674892216089829fccd1
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2019-06-18 22:34:12 -0700

    Fixed blender rope initialization

--- rope-blender.py
+++ rope-blender.py
@@ -6,12 +6,12 @@ import random
 
 
 class RopeRenderer:
-    def __init__(self, rope_radius=0.1, rope_screw_offset=8, rope_length=10, bezier_scale=7, bezier_subdivisions=10):
-        self.rope_radius = rope_radius
-        self.rope_length = rope_length
-        self.rope_screw_offset = rope_screw_offset
-        self.bezier_scale = bezier_scale
-        self.bezier_subdivisions = bezier_subdivisions
+    def __init__(self, rope_radius=0.1, rope_screw_offset=2, rope_iterations=10, bezier_scale=7, bezier_subdivisions=10):
+        self.rope_radius = rope_radius # thickness of rope
+        self.rope_iterations = rope_iterations # how many "screws" are stacked lengthwise to create the rope
+        self.rope_screw_offset = rope_screw_offset # how tightly wound the "screw" is
+        self.bezier_scale = bezier_scale # length of bezier curve
+        self.bezier_subdivisions = bezier_subdivisions # the number of splits in the bezier curve (ctrl points - 1)
         self.origin = (0, 0, 0)
         self.rope = None
         self.asymm_rope = None
@@ -44,7 +44,7 @@ class RopeRenderer:
         self.rope.name = self.rope_name
         bpy.ops.object.modifier_add(type='SCREW')
         self.rope.modifiers["Screw"].screw_offset = self.rope_screw_offset
-        self.rope.modifiers["Screw"].iterations = self.rope_length
+        self.rope.modifiers["Screw"].iterations = self.rope_iterations
         bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Screw")
 
     def add_rope_asymmetry(self):
@@ -52,7 +52,7 @@ class RopeRenderer:
         bpy.ops.mesh.primitive_cone_add(location=(self.origin[0], self.origin[1] + 0.32, self.origin[2]))
         bpy.ops.transform.resize(value=(0.5, 0.5, 0.5))
         bpy.ops.transform.rotate(value= pi / 2, orient_axis='X')
-        bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(self.origin[0], self.origin[1], self.origin[2] + self.rope_radius*self.rope_screw_offset*self.rope_length))
+        bpy.ops.mesh.primitive_ico_sphere_add(radius=0.5, location=(self.origin[0], self.origin[1], self.origin[2] + self.rope_radius*self.rope_screw_offset*self.rope_iterations))
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.join()
         self.rope_asymm = bpy.context.active_object
